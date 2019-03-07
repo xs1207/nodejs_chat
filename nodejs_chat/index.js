a6ac0e2165c57da62eda9c8def2bd10a5d6f3744
dukailong <dukailong@localhost.localdomain>
2019-03-07 02:24:30 +0000

    端口

--- nodejs_chat/index.js
+++ nodejs_chat/index.js
@@ -21,6 +21,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3001, function(){
-    console.log('listening on *:3001');
+http.listen(3005, function(){
+    console.log('listening on *:3005');
 });
